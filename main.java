dc1a4d9d66933cba782444b5d06b418e2924d693
Oscar Rodriguez <oscardelangelr@gmail.com>
2025-04-14 13:24:46 -0600

    Tarea: Se hicieron private los atributos para incluir sus getter y setter de cada atributo en Persona.java y en Main.java se imprimió ocupando el getter

--- main.java
+++ main.java
@@ -1,11 +1,24 @@
-public class main {
+public class Main {
     public static void main(String[] args) {
-        //Nota: en el main se pone los valores que quieres en el constructor, pero en la clase se define el constructor
-        persona p = new persona("Carlos",25,1.80,"Masculino", "Italiano");
+        // Nota: en el main se pone los valores que quieres en el constructor, pero en
+        // la clase se define el constructor
+        Persona p = new Persona("Carlos", 25, 1.80, "Masculino", "Italiano");
+        
+        /*set: Se usa para modificar el valor del atributo
+          get: Se usa para obtener el valor del atríbulo, para leerlo e imprimirlo*/
+
+        /*Cuando ponemos los parámetros en privado y creamos los getter y setter se ocupa el getter para imprimirlo: */
+        // Se modifica el nombre con el setter (esto es opcional):
+        p.setNombre("Enrique");
+
+        // Después se imprime el nombre con el getter:
+        System.out.println("El papu se llama: " + p.getNombre());
+        System.out.println("Tiene " + p.getEdad() + " añitos");
+        System.out.println("Mide: " + p.getAltura() + " metros, crack");
 
         p.hablar();
         p.caminar();
         p.comer();
-        p.dormir(); 
+        p.dormir();
     }
 }
--- persona.java
+++ persona.java
@@ -1,12 +1,13 @@
-public class persona{
-    String nombre;
-    int edad;
-    double altura;
-    String genero;
-    String nacionalidad;
+public class Persona {
 
-//aqui se define el constructor para que funcione el main, si no lo tiene no va a funcionar
-    public persona(String nombre, int edad, double altura, String genero, String nacionalidad){
+    private String nombre;
+    private int edad;
+    private double altura;
+    private String genero;
+    private String nacionalidad;
+
+    // aqui se define el constructor para que funcione el main, si no lo tiene no va a funcionar
+    public Persona(String nombre, int edad, double altura, String genero, String nacionalidad) {
         this.nombre = nombre;
         this.edad = edad;
         this.altura = altura;
@@ -14,20 +15,67 @@ public class persona{
         this.nacionalidad = nacionalidad;
     }
 
+    // getter, estructura del getter:
+    /* public Tipo getNombreDelAtributo() {
+       return this.nombreDelAtributo;
+       }*/
+    public String getNombre() {
+        return this.nombre;
+    }
 
-    void hablar(){
-    System.out.println(nombre + " te dijo hola");
-}
+    public int getEdad() {
+        return this.edad;
+    }
 
-    void caminar(){
+    public double getAltura() {
+        return this.altura;
+    }
+
+    public String getGenero() {
+        return this.genero;
+    }
+
+    public String getNacionalidad() {
+        return this.nacionalidad;
+    }
+
+    // Setter, estructura del setter:
+    /* public void setNombreDelAtributo(Tipo nombreDelAtributo) {
+       this.nombreDelAtributo = nombreDelAtributo;
+     * }*/
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    void hablar() {
+        System.out.println(nombre + " te dijo hola");
+    }
+
+    void caminar() {
         System.out.println(nombre + " está llegando al parque a ver a su ex con otro");
     }
 
-    void comer(){
+    void comer() {
         System.out.println(nombre + " está viendo como está comiendo con otro");
     }
 
-    void dormir(){
+    void dormir() {
         System.out.println(nombre + " está soñando con ella");
     }
 
